ed1dbe5dc0c5e9f686a73809d590b2c3877b3fe4
Věroš Kaplan <921096+VerosK@users.noreply.github.com>
2025-03-27 16:15:53 +0100

    fix: remove lazy page loading
     * Links header was removed by Fakturoid in API v3

--- fakturoid/paging.py
+++ fakturoid/paging.py
@@ -1,27 +1,38 @@
+import itertools
 from itertools import islice
 
 from fakturoid import six
 
 
 class PagedResource(object):
-    """List adapter for paged resources. Returns sliceable lazy loaded object."""
+    """List adapter for paged resources. Do not use it unless required
+       as it loads all the pages."""
 
     def __init__(self):
+        self.objects = None
         self.pages = {}
-        self.page_size = None
-        self.page_count = None
+        self.item_count = None
 
     def load_page(self, n):
         raise NotImplementedError("You must implement load_page method.")
 
-    def ensure_paging_info(self):
-        if self.page_count is None or self.page_size is None:
-            # load any page to get page count and page size
-            self.get_page(0)
+    def load_all_pages(self):
+        self.objects = []
+        self.item_count = 0
+        for n in itertools.count():
+            try:
+                page = self.get_page(n)
+                self.objects.extend(page)
+                self.item_count += len(page)
+            except IndexError:
+                # we've reached the end of list
+                break
 
+    def ensure_all_pages(self):
+        if not self.objects:
+            self.load_all_pages()
+            
     def get_page(self, n):
-        if self.page_count and n >= self.page_count:
-            raise IndexError('index out of range')
         if n in self.pages:
             return self.pages[n]
         page = self.load_page(n)
@@ -31,19 +42,18 @@ class PagedResource(object):
         raise IndexError('index out of range')
 
     def __len__(self):
-        self.ensure_paging_info()
-        return (self.page_size * (self.page_count - 1) +
-                len(self.get_page(self.page_count - 1)))
+        self.ensure_all_pages()
+        return self.item_count
+
 
     def __getitem__(self, key):
-        self.ensure_paging_info()
+        self.ensure_all_pages()
         if isinstance(key, int):
             if key < 0:
                 key = len(self) + key
                 if key < 0:
                     raise IndexError('index out of range')
-            page_n, idx = divmod(key, self.page_size)
-            return self.get_page(page_n)[idx]
+            return self.objects[key]
         elif isinstance(key, slice):
             # TODO support negative step
             return islice(self, *key.indices(len(self)))
@@ -63,13 +73,13 @@ class ModelList(PagedResource, six.UnicodeMixin):
         params = {'page': n + 1}
         params.update(self.params)
         response = self.model_api.session._get(self.endpoint, params=params)
-        if self.page_count is None:
-            self.page_count = response.get('page_count', n + 1)
         objects = list(self.model_api.unpack(response))
-        if self.page_size is None:
-            self.page_size = len(objects)
         return objects
 
     def __unicode__(self):
         # TODO print if loaded
-        return "<list of {0} models>".format(self.model_api.model_type.__name__)
+        if self.objects:
+            return "<list of {0} models ({1} items)>".format(
+                self.model_api.model_type.__name__, len(self))
+        else:
+            return "<list of {0} models>".format(self.model_api.model_type.__name__)
